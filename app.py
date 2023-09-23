51acc506bcaa2b4229f334a90978d56a770dfcbc
vinayak <vinayakshukla1239@gmail.com>
2023-09-23 20:11:04 +0530

    streamlit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(
+    page_icon="🧊",
+    page_title="Diwali sales analysis",
+    layout="wide")
